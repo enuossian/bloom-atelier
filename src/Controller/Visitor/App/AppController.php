07e97c30f82407a88ceb1b4e853f69830d87e170
Naissoune <naissounem@email.com>
2026-03-22 01:00:29 +0100

    add:mise en forme de la navbar et de la hero section

--- src/Controller/Visitor/App/AppController.php
+++ src/Controller/Visitor/App/AppController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller\Visitor\App;
 
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AppController extends AbstractController
 {
+    public function __construct(
+        private readonly ServiceRepository $serviceRepository,
+    ) {
+    }
+
     #[Route('/', name: 'visitor_home_index')]
     public function index(): Response
     {
-        return $this->render('pages/visitor/app/index.html.twig');
+        $services = $this->serviceRepository->findAll();
+
+        return $this->render('pages/visitor/app/index.html.twig', [
+            'services' => $services,
+        ]);
     }
 
     #[Route('/notre-concept', name: 'visitor_home_concept')]
