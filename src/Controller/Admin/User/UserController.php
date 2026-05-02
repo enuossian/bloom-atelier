fd080e242cf07faa35f381ef688334372e9e8b28
Naissoune <naissounem@email.com>
2026-05-02 14:54:15 +0200

    add: supprimer le scroll après le commentaire

--- src/Controller/Admin/User/UserController.php
+++ src/Controller/Admin/User/UserController.php
@@ -18,7 +18,6 @@ final class UserController extends AbstractController
     public function __construct(
         private readonly UserRepository $userRepository,
         private readonly EntityManagerInterface $entityManager,
-        private readonly TokenStorageInterface $tokenStorage,
     ) {
     }
 
